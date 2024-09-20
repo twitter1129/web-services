a498b92cfa415356868bdf7f81129733d83c5fc8
Cloud User <ec2-user@ip-172-31-17-1.eu-central-1.compute.internal>
2024-09-20 18:32:52 +0000

    New app name

--- app.java
+++ app.java
@@ -2,4 +2,6 @@
 #This code is for web application
 #make use of variables and dynamic variables
 #author: ONU
-#This script has only READ access to non team membersi 
+#This script has only READ access to non team members
+#app name=$name
+echo END
